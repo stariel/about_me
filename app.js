20dc62a50068d89a099fe7c56a9b95aef2930569
stariel <ariel.altaras@gmail.com>
2017-08-08 14:36:01 -0700

    Got first question working in app.js!

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var q1 = 'Is Ariel an only child?';
+var a1 = prompt(q1);
+console.log('Q: ' + q1);
+console.log('A:' + a1);
+var a1l = a1.toLowerCase();
+if (a1l === 'yes') {
+  alert ('Sorry, Ariel has two siblings.');
+}
+else {
+  alert ('Correct! Ariel has two siblings.');
+}
