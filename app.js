ba62b2b914b2d262c192fa7253dd639475db3f0e
stariel <ariel.altaras@gmail.com>
2017-08-08 16:19:13 -0700

    Better js, aw yeah!

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var q1 = 'Is Ariel an only child?';
 var a1 = prompt(q1);
 console.log('Q: ' + q1);
 console.log('A:' + a1);
-if (a1.toLowerCase() === 'yes') {
+if (a1.toLowerCase() === 'yes' || 'y' ) {
   alert ('Sorry, Ariel has two siblings.');
 }
-else if (a1.toLowerCase() === 'no') {
+else if (a1.toLowerCase() === 'no' || 'n') {
   alert ('Correct! Ariel has two siblings.');
 }
 else {
@@ -18,10 +18,10 @@ var q2 = 'Does Ariel like knitting?';
 var a2 = prompt(q2);
 console.log('Q: ' + q2);
 console.log('A:' + a2);
-if (a2.toLowerCase() === 'yes') {
+if (a2.toLowerCase() === 'yes' || 'y') {
   alert ('You\'re right! Ariel is crazy about knitting.');
 }
-else if (a2.toLowerCase() === 'no') {
+else if (a2.toLowerCase() === 'no' || 'n') {
   alert ('You must not know her, knitting is Ariel\'s favorite hobby.');
 }
 else {
@@ -32,10 +32,10 @@ var q3 = 'Is Ariel\'s favorite music genre rap?';
 var a3 = prompt(q3);
 console.log('Q: ' + q3);
 console.log('A:' + a3);
-if (a3.toLowerCase() === 'yes') {
+if (a3.toLowerCase() === 'yes' || 'y') {
   alert ('No way! Ariel hates rap.');
 }
-else if (a3.toLowerCase() === 'no') {
+else if (a3.toLowerCase() === 'no' || 'n') {
   alert ('You\'re right, Ariel loves metal music.');
 }
 else {
@@ -46,10 +46,10 @@ var q4 = 'Is Ariel Canadian?';
 var a4 = prompt(q4);
 console.log('Q: ' + q4);
 console.log('A:' + a4);
-if (a4.toLowerCase() === 'yes') {
+if (a4.toLowerCase() === 'yes' || 'y') {
   alert ('No, Ariel grew up in Seattle.');
 }
-else if (a4.toLowerCase() === 'no') {
+else if (a4.toLowerCase() === 'no' || 'n') {
   alert ('Correct. Ariel is from Seattle!');
 }
 else {
@@ -60,10 +60,10 @@ var q5 = 'Does Ariel have pets?';
 var a5 = prompt(q5);
 console.log('Q: ' + q5);
 console.log('A:' + a5);
-if (a5.toLowerCase() === 'yes') {
+if (a5.toLowerCase() === 'yes' || 'y') {
   alert ('Right, Ariel has two cats.');
 }
-else if (a5.toLowerCase() === 'no') {
+else if (a5.toLowerCase() === 'no' || 'n') {
   alert ('Sorry, Ariel has two cats.');
 }
 else {
